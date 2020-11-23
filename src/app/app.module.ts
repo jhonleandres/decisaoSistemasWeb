d155056f609d68efe2371ae6a90b94666b117dc5
JHONLEANDRES BARBOSA DA SILVA <jhonleandres.silva@bretas.com.br>
2020-11-23 16:30:33 -0300

    configurando auth e url api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './account/login/login.component';
+import { CreateAccountComponent } from './account/create-account/create-account.component';
+import { HomeComponent } from './layout/home/home.component';
+import { AuthenticationComponent } from './layout/authentication/authentication.component';
+import { TaskCreateComponent } from './activities/tasks/task-create/task-create.component';
+import { TaskUpdateComponent } from './activities/tasks/task-update/task-update.component';
+import { TaskListComponent } from './activities/tasks/task-list/task-list.component';
+import { TaskStatusComponent } from './activities/tasks/task-status/task-status.component';
+import { TaskRemoveComponent } from './activities/tasks/task-remove/task-remove.component';
+import { ActivitiesComponent } from './activities/activities.component';
+
+import { httpInterceptorProviders } from './http-interceptors';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    CreateAccountComponent,
+    HomeComponent,
+    AuthenticationComponent,
+    TaskCreateComponent,
+    TaskUpdateComponent,
+    TaskListComponent,
+    TaskStatusComponent,
+    TaskRemoveComponent,
+    ActivitiesComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    FormsModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    httpInterceptorProviders
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
